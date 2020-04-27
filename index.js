55c0e23c14f49b035bf2aadfdc333838f536d7bb
Brooke Luro <lurob@me.com>
2020-04-27 19:51:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,15 @@ function logShout(string) {
 function logWhisper(string) {
   console.log("HELLO".toLowerCase())
 }
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === lowercase) {
+    return "I can't hear you"
+  } else if (string.toUpperCase() === uppercase) {
+    return "YES INDEED!" 
+  } else ("I love you, Grandma." === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+  }
+  }  
+})
